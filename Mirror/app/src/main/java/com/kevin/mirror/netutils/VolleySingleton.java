380cb584b9fa9f73b0e12a6dfb2a36e75c820334
kevin <kevinpark@163.com>
2016-06-20 15:28:49 +0800

    base+liteorm+volley

---
+++ Mirror/app/src/main/java/com/kevin/mirror/netutils/VolleySingleton.java
@@ -0,0 +1,33 @@
+package com.kevin.mirror.netutils;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+import com.kevin.mirror.MyApp;
+
+/**
+ * Created by kevin on 16/6/20.
+ */
+public class VolleySingleton {
+    private static VolleySingleton ourInstance = new VolleySingleton();
+    private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
+
+    public static VolleySingleton getInstance() {
+
+        return ourInstance;
+    }
+
+    private VolleySingleton() {
+        requestQueue= Volley.newRequestQueue(MyApp.context);
+        imageLoader=new ImageLoader(requestQueue,new MemoryCache());
+    }
+
+    public RequestQueue getRequestQueue() {
+        return requestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        return imageLoader;
+    }
+}
