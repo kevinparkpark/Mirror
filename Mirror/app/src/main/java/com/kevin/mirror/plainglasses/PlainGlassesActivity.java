c74631a422b671593c9015ac264f2d8040258ed9
yanghao <996044098@qq.com>
2016-06-21 14:06:10 +0800

    无意义的更改

---
+++ Mirror/app/src/main/java/com/kevin/mirror/plainglasses/PlainGlassesActivity.java
@@ -0,0 +1,19 @@
+package com.kevin.mirror.plainglasses;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+/**
+ * Created by dllo on 16/6/20.
+ */
+public class PlainGlassesActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+}
