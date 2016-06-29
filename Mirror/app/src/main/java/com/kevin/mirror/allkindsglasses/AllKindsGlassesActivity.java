8566e6ca0621df7f37ff9df1679a127a773ec6cf
yanghao <996044098@qq.com>
2016-06-29 10:47:41 +0800

    aa

--- Mirror/app/src/main/java/com/kevin/mirror/allkindsglasses/AllKindsGlassesActivity.java
+++ Mirror/app/src/main/java/com/kevin/mirror/allkindsglasses/AllKindsGlassesActivity.java
@@ -9,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.AbsListView;
@@ -71,6 +72,8 @@ public class AllKindsGlassesActivity extends BaseActivity implements View.OnClic
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ALLKINDS=this;
+
+       // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
     }
 
     @Override
